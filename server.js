d795024ed3922fd9f5c8a622396d7914d7818cd1
Spike Nox <spike01@gmail.com>
2014-10-16 14:53:38 +0100

    Server up and working

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var server = express();
+
+server.get('/', function(request, response){
+  response.send('hello world');
+});
+
+server.listen(3000, function(){
+  console.log("Listening on port 3000");
+});
+
---
+++ test/ServerSpec.js
@@ -0,0 +1,15 @@
+var server = require('../server');
+chai = require('chai')
+expect = chai.expect
+var Browser = require('zombie')
+
+describe('homepage', function() {
+
+ var browser = new Browser({ site: 'http://localhost:3000' }); 
+
+  it('Returns response code 200 when visiting the homepage', function(){
+    browser.visit('/', function(){
+      expect(browser.success).to.be.true
+    });
+  });
+});
