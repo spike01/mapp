b804a277dc959a82d08d7607b60d0a67cb4bf346
Spike Nox <spike01@gmail.com>
2014-10-17 19:10:53 +0100

    Moves formatterSpec into node

---
+++ spec/node/formatterSpec.js
@@ -0,0 +1,22 @@
+formatter = require('../../src/formatter.js')
+sampleTweet = require('./specHelper.js')
+
+describe("Formatter", function() {
+
+  it("should output the text value of a tweet", function() {
+    expect(formatter.format(sampleTweet.unformattedTweet).text).toEqual(unformattedTweet.text);
+  })
+
+  it("should output the coordinates of a tweet so that there are no negative values", function() {
+    expect(formatter.format(sampleTweet.unformattedTweet).coords).toEqual([ 63.84097, 207.92473 ]);
+  })
+
+  it("should output the language of a tweet", function() {
+    expect(formatter.format(sampleTweet.unformattedTweet).lang).toEqual(unformattedTweet.lang);
+  })
+
+  it("should output a sentiment value of 0", function() {
+    expect(formatter.format(sampleTweet.unformattedTweet).sentiment).toEqual(0);
+  })
+
+})
--- src/formatter.js
+++ src/formatter.js
@@ -1,4 +1,4 @@
-var format = function format(tweet){
+this.format = function (tweet){
   return  { text: tweet.text,
             coords: [ tweet.geo.coordinates[0]+90, tweet.geo.coordinates[1]+180 ],
             lang: tweet.lang,
@@ -6,4 +6,3 @@ var format = function format(tweet){
           };
 }
 
-module.exports = format;
