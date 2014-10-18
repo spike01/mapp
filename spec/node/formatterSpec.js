79a2baeecf5f9366e8a2383c205cf4ea2e4922de
Ethel Ng <ethel.ng@gmail.com>
2014-10-18 12:21:09 +0100

    Refactor to remove unnecessary chaining

--- spec/node/formatterSpec.js
+++ spec/node/formatterSpec.js
@@ -4,19 +4,19 @@ sampleTweet = require('./specHelper.js')
 describe("Formatter", function() {
 
   it("should output the text value of a tweet", function() {
-    expect(formatter.format(sampleTweet.unformattedTweet).text).toEqual(unformattedTweet.text);
+    expect(formatter.format(unformattedTweet).text).toEqual(unformattedTweet.text);
   })
 
   it("should output the coordinates of a tweet so that there are no negative values", function() {
-    expect(formatter.format(sampleTweet.unformattedTweet).coords).toEqual([ 63.84097, 207.92473 ]);
+    expect(formatter.format(unformattedTweet).coords).toEqual([ 63.84097, 207.92473 ]);
   })
 
   it("should output the language of a tweet", function() {
-    expect(formatter.format(sampleTweet.unformattedTweet).lang).toEqual(unformattedTweet.lang);
+    expect(formatter.format(unformattedTweet).lang).toEqual(unformattedTweet.lang);
   })
 
   it("should output a sentiment value of 0", function() {
-    expect(formatter.format(sampleTweet.unformattedTweet).sentiment).toEqual(0);
+    expect(formatter.format(unformattedTweet).sentiment).toEqual(0);
   })
 
 })
--- spec/node/specHelper.js
+++ spec/node/specHelper.js
@@ -1,46 +1,46 @@
-this.acceptingTweet = {
+exports.acceptingTweet = {
   text: "accepting stuff",
   coords: [ -26.15903, 27.92473 ],
   lang: 'en',
   sentiment: 0
 }
 
-this.happyTweet = {
+exports.happyTweet = {
   text: "happy stuff",
   coords: [ -26.15903, 27.92473 ],
   lang: 'en',
   sentiment: 0
 }
 
-this.sadTweet = {
+exports.sadTweet = {
   text: "sad stuff",
   coords: [ -26.15903, 27.92473 ],
   lang: 'en',
   sentiment: 0
 }
 
-this.weirdTweet = {
+exports.weirdTweet = {
   text: "happy accepting torture",
   coords: [ -26.15903, 27.92473 ],
   lang: 'en',
   sentiment: 0
 }
 
-this.pissedOffSpanishTweet = {
+exports.pissedOffSpanishTweet = {
   text: "¿por que carajos soy un pendejo?",
   coords: [ 40.467812, -3.874747 ],
   lang: 'es',
   sentiment: 0
 }
 
-this.turkishTweet = {
+exports.turkishTweet = {
   text: 'İşi bırakmak şart olduysa gitmemek için bi engel yok .',
   coords: [ 41.076211, 29.012198 ],
   lang: 'tr',
   sentiment: 0
 }
 
-this.unformattedTweet = { created_at: 'Thu Oct 16 10:50:02 +0000 2014',
+exports.unformattedTweet = { created_at: 'Thu Oct 16 10:50:02 +0000 2014',
   id: 522700992320929800,
   id_str: '522700992320929792',
   text: 'Mine &gt; Cheeseboysa "@Blaklez: iG: Blaklez_SA"',
