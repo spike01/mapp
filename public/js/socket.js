d38946603440e4cf04b9e9e40b018671e867598f
ElliotLewis8923 <elewis8923@gmail.com>
2014-10-20 15:15:30 +0100

    canvas now resizes and zooms

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,36 +1,37 @@
 $(document).ready(function(){
 
   var socket = io.connect('/');
-  var dataStore = [];
-  var viewportWidth, viewportHeight, width, height, ratio, radius, x, y
 
-  function setVars()  {
+  var dataStore = [];
+  var canvas, x, y;
+  var viewportWidth = window.innerWidth;
+  var viewportHeight = window.innerHeight;
+  var width = viewportWidth;
+  var height = viewportWidth / 2;
+  var ratio = viewportWidth / 360;
+
+  window.onload = window.onresize = function() {
+
     viewportWidth = window.innerWidth;
     viewportHeight = window.innerHeight;
     width = viewportWidth;
     height = viewportWidth / 2;
     ratio = viewportWidth / 360;
-    radius = viewportWidth / 700;
+
     x = d3.scale.linear()
       .domain([0, width])
       .range([0, width]);
 
     y = d3.scale.linear()
       .domain([0, height])
-      .range([height, 0]); 
-  }
+      .range([height, 0]);
 
-  setVars()
-
-  var canvas = d3.select("#container").append("canvas")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("id", "map")
-    .call(d3.behavior.zoom().x(x).y(y).scaleExtent([1, 8]).on("zoom", redraw))
-    .node().getContext("2d");
-
-  window.onload = window.onresize = function() {
-    setVars();
+    canvas = d3.select("canvas")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("id", 'map')
+      .call(d3.behavior.zoom().x(x).y(y).scaleExtent([1, 8]).on("zoom", zoom))
+      .node().getContext("2d");
 
     var map = document.getElementById('map')
 
@@ -39,13 +40,11 @@ $(document).ready(function(){
     map.style.position = "fixed";
     map.style.top = (viewportHeight - height) / 2;
     map.style.left = (viewportWidth - width) / 2;
-    map.call(d3.behavior.zoom().x(x).y(y).scaleExtent([1, 8]).on("zoom", redraw))
-    map.node().getContext("2d");
 
-    redraw()
+    zoom()
   }
 
-  function redraw() {
+  function zoom() {
     canvas.clearRect(0, 0, width, height);
     draw();
   }
@@ -59,7 +58,7 @@ $(document).ready(function(){
       cy = y(d[1]*ratio);
       canvas.fillStyle = d[2] 
       canvas.moveTo(cx, cy)
-      canvas.arc(cx, cy, radius, 0, 2 * Math.PI);
+      canvas.arc(cx, cy, 2.5, 0, 2 * Math.PI);
       canvas.fill()
       canvas.closePath();
     }
@@ -75,7 +74,7 @@ $(document).ready(function(){
     canvas.beginPath();
     cx = x((data.coords[1]*ratio));
     cy = y((data.coords[0])*ratio);
-    canvas.arc(cx, cy, radius, 0, 2 * Math.PI, false);
+    canvas.arc(cx, cy, 2, 0, 2 * Math.PI, false);
     canvas.fillStyle = data.colour;
     canvas.fill();
     canvas.closePath();
