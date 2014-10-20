4e454be9c8ebc3f21b62341cf812f81bcb8397aa
ElliotLewis8923 <elewis8923@gmail.com>
2014-10-20 13:51:28 +0100

    cleaned up colourPicker module, removed console logs

--- src/colourPicker.js
+++ src/colourPicker.js
@@ -1,24 +1,17 @@
-function componentToHex(c) {
+exports.pickColour = function(sentiment) {
+	colour = 0.0;
+	sentiment = Math.round(((sentiment + 5.0) / 10.0) * 255);
+  colour = _rgbToHex(255, sentiment, 0);
+  return colour
+}
+
+//private
+
+function _componentToHex(c) {
     var hex = c.toString(16);
     return hex.length == 1 ? "0" + hex : hex;
 }
 
-function rgbToHex(r, g, b) {
+function _rgbToHex(r, g, b) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-
-exports.pickColour = function(sentiment) {
-	colour = 0.0;
-	sentiment = Math.round(((sentiment + 5.0) / 10.0) * 255);
-	//inverseSentiment = 255 - sentiment
-    colour = rgbToHex(255, sentiment, 0);
-    console.log(sentiment);
-    //console.log(inverseSentiment);
-    console.log('---' + colour);
-  // if(sentiment < 0) {
-  // 	sentiment *= -1;
-  //   colour = rgbToHex(inverseSentiment, 0, sentiment);
-  // } 
-
-  return colour
 }
