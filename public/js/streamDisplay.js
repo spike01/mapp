1506743352a175e42bca7ddfb36def5e9eca28c0
ElliotLewis8923 <elewis8923@gmail.com>
2014-10-21 20:10:01 +0100

    changed css and streamDisplay

--- public/js/streamDisplay.js
+++ public/js/streamDisplay.js
@@ -2,10 +2,11 @@ var counter = 0;
 
 function streamDisplay(data) {
 	counter += 1;
-	if (counter % 20 === 0) {
+	if (counter % 100 === 0) {
 		var text = data.text;
 		var username = data.username;
-		$('.tweetStreamDisplay ul').prepend('<li>' + '<b>@' + username + '</b> ' + text + '</li>');
+		$('<li>' + '<b>@' + username + '</b> ' + text + '</li>').prependTo.('.tweetStreamDisplay ul').fadeIn('slow');
+			$('ul li:nth-child(3)').css('opacity', '0');
 
 	}
 
--- src/colourPicker.js
+++ src/colourPicker.js
@@ -1,7 +1,9 @@
 exports.pickColour = function(sentiment) {
 	var colour = 0.0;
-	sentiment = Math.round(((sentiment + 5.0) / 10.0) * 255);
-  colour = _rgbToHex(255, sentiment, 0);
+	if (sentiment < 0) {
+	sentiment = Math.round(((sentiment + 5.0) / 10.0) * 255)
+  	colour = _rgbToHex(255, sentiment, 0) }
+
   return colour;
 };
 
