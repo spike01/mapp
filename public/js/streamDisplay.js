ce01635a396f739bacccac889162aaa9750df4c7
Spike Nox <spike01@gmail.com>
2014-10-21 21:02:27 +0100

    Single tweet display works but doesn't fade out

--- public/js/streamDisplay.js
+++ public/js/streamDisplay.js
@@ -2,11 +2,13 @@ var counter = 0;
 
 function streamDisplay(data) {
 	counter += 1;
-	if (counter % 100 === 0 && stopped === false) {
+	if (counter % 300 === 0 && stopped === false) {
 		var text = data.text;
 		var username = data.username;
 		$('<li>' + '<b>@' + username + ':</b> ' + text + '</li>').prependTo('.tweetStreamDisplay ul').fadeIn("slow");
-			$('ul li:nth-child(3)').css('opacity', '0');
+			$('.tweetStreamDisplay li:gt(0)').fadeOut(500, function(){
+        this.remove();
+      });
   }	
 }
 
