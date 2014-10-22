f36e55310e59cf8865a2542ef6e26c66357f89ef
silver-io <javier@silver.io>
2014-10-22 16:35:23 +0100

    tests with animate.css

--- public/js/about.js
+++ public/js/about.js
@@ -1,5 +1,22 @@
-$("a").click(function(){
+// $("a").click(function(){
+//   $("a").animate({
+//     height:'toggle'
+//   });
+// });
+
+$(document).ready(function(){
+$("a").hide();
+
+// setTimeout(function(){
+
+//   $("a").show().fadeIn('slow')
+
+
+// },1000)
+
   $("a").animate({
     height:'toggle'
   });
+
+
 });
