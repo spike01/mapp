ddcdcf42104e3a52170e0186d2a0aa67396bdb91
silver-io <javier@silver.io>
2014-10-22 09:13:19 +0100

    making the about dissappear

--- public/js/about.js
+++ public/js/about.js
@@ -1,4 +1,4 @@
-$( "a" ).click(function( event ) {
-  event.preventDefault();
-  $( this ).hide();
-});
+// $( "a" ).click(function( event ) {
+//   event.preventDefault();
+//   $( this ).hide();
+// });
