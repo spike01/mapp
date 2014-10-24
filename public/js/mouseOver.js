3d872307a8d06f85fbafb622388d08d71081de93
Spike Nox <spike01@gmail.com>
2014-10-24 10:46:49 +0100

    Fixes tests

--- public/js/mouseOver.js
+++ public/js/mouseOver.js
@@ -1,27 +1,27 @@
-var mouseCanvas = document.getElementById('map');
+//var mouseCanvas = document.getElementById('map');
 
-function getMousePos(canvas, evt) {
-  var rect = mouseCanvas.getBoundingClientRect();
-  return {
-    x: evt.clientX - rect.left,
-      y: evt.clientY - rect.top
-  };
-}
+//function getMousePos(canvas, evt) {
+  //var rect = mouseCanvas.getBoundingClientRect();
+  //return {
+    //x: evt.clientX - rect.left,
+      //y: evt.clientY - rect.top
+  //};
+//}
 
-var mouseCoords = [];
+//var mouseCoords = [];
 
-mouseCanvas.addEventListener('mousemove', function(evt) {
-  console.log([mouseCoords[0], mouseCoords[1]]);
-  var mousePos = getMousePos(mouseCanvas, evt);
-  mouseCoords = [Math.round(mousePos.x), Math.round(mousePos.y)]
-  dataStore.forEach(function(coords) {
-    if(Math.round(coords[0]) === mouseCoords[0] && (Math.round(coords[1])) === mouseCoords[1]) {
-        console.log('gotcha');
-    }
-  });
-});
+//mouseCanvas.addEventListener('mousemove', function(evt) {
+  //console.log([mouseCoords[0], mouseCoords[1]]);
+  //var mousePos = getMousePos(mouseCanvas, evt);
+  //mouseCoords = [Math.round(mousePos.x), Math.round(mousePos.y)]
+  //dataStore.forEach(function(coords) {
+    //if(Math.round(coords[0]) === mouseCoords[0] && (Math.round(coords[1])) === mouseCoords[1]) {
+        //console.log('gotcha');
+    //}
+  //});
+//});
   
-var map = document.getElementById("map")
+//var map = document.getElementById("map")
 
-map.setAttribute("width", width);
-map.setAttribute("height", height);
+//map.setAttribute("width", width);
+//map.setAttribute("height", height);
