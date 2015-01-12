1513941ae83d99c0d021d321b128981bd0441703
Spike Nox <spike01@gmail.com>
2015-01-12 23:40:13 +0000

    Removes weird 404 test - closes #1

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -13,15 +13,15 @@ describe('homepage', function() {
     browser = new Browser({ site: 'http://localhost:3000' });
   });
 
-  before(function(done) {
-    browser.visit('/', done);
+  describe('when visiting the homepage', function(){
+
+    before(function(done) {
+      browser.visit('/', done);
+    });
+
+    it('Returns response code 200', function(){
+      expect(browser.success).to.be.true
+    });
   });
 
-  it('Returns response code 200 when visiting the homepage', function(){
-      expect(browser.success).to.be.true
-  });
-
-  it('Returns response code 404 when visiting a page not listed in the routes', function(){
-      expect(browser.success).to.be.true
-  });
 });
